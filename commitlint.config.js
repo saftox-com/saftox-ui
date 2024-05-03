ca4187a7117d659904b4b92a0e9c793d32428cb4
Serhii Zaitsev <ceo@saftox.com>
2024-05-03 12:44:18 +0300

    chore(commitlint): extend commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  plugins: ["commitlint-plugin-function-rules"],
+  helpUrl: "https://github.com/saftox-com/saftox-ui/blob/main/CONTRIBUTING.md#commit-convention",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "feature", "fix", "refactor", "docs", "build", "test", "ci", "chore"],
+    ],
+    "function-rules/header-max-length": [0],
+  },
 };
