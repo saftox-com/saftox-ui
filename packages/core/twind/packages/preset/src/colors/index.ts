79f52118ed5051fb5ee79a8ca71b330de83cd42d
Serhii Zaitsev <ceo@saftox.com>
2024-05-20 16:27:10 +0300

    feat(core): custom preset for twind

---
+++ packages/core/twind/packages/preset/src/colors/index.ts
@@ -0,0 +1,11 @@
+import { commonColors } from "./common";
+import { themeColorsLight } from "./semantic";
+
+export * from "./types";
+
+const colors = {
+	...commonColors,
+	...themeColorsLight,
+};
+
+export { colors, commonColors };
---
+++ packages/core/twind/packages/preset/src/tokens/screens.ts
@@ -0,0 +1,12 @@
+export default {
+	mobile: "640px",
+	tablet: "768px",
+	laptop: "1024px",
+	desktop: "1280px",
+	large_desktop: "1536px",
+	sm: "640px",
+	md: "768px",
+	lg: "1024px",
+	xl: "1280px",
+	xxl: "1280px",
+};
