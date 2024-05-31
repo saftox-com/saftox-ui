e82e6c61b504a645c0df6b82b168cad75e80524d
Serhii Zaitsev <ceo@saftox.com>
2024-05-31 15:32:19 +0300

    refactor(storybook): remove async paths to stories

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -2,17 +2,6 @@ import type { StorybookConfig } from "storybook-solidjs-vite";
 
 import { dirname, join, resolve } from "node:path";
 
-const fg = require("fast-glob");
-
-const getStories = () =>
-	fg.sync([
-		resolve(
-			__dirname,
-			"../../components/**/stories/**/*.stories.@(js|jsx|ts|tsx)",
-		),
-		resolve(__dirname, "../../components/**/stories/*.stories.mdx"),
-		"!**/node_modules",
-	]);
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
@@ -21,7 +10,10 @@ function getAbsolutePath(value: string): any {
 	return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-	stories: async () => [...getStories()],
+	stories: [
+		"../../components/**/stories/**/*.stories.@(js|jsx|ts|tsx)",
+		"../../core/theme/stories/*.stories.@(js|jsx|ts|tsx)",
+	],
 	addons: [
 		getAbsolutePath("storybook-dark-mode"),
 		getAbsolutePath("@storybook/addon-a11y"),
