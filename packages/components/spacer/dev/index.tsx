58e977741a50357f3429f05ca4d760815bae5db0
Serhii Zaitsev <ceo@saftox.com>
2024-06-12 02:44:51 +0300

    feat(cmp): new spacer package

---
+++ packages/components/spacer/dev/index.tsx
@@ -0,0 +1,14 @@
+import { render } from "solid-js/web";
+import { Spacer } from "../src";
+
+const App = () => {
+	return (
+		<>
+			<Spacer>
+				<span>Hello Solid</span>
+			</Spacer>
+		</>
+	);
+};
+
+render(() => <App />, document.getElementById("root")!);
