e2b43872e5e8a15bfff308c82de71cfb6fb00cdb
Serhii Zaitsev <ceo@saftox.com>
2024-06-14 14:10:22 +0300

    feat(shared-hooks): add helpers for hooks

---
+++ packages/utilities/shared-hooks/src/use-supported/index.ts
@@ -0,0 +1,16 @@
+import type { Accessor } from "solid-js";
+
+import { createMemo } from "solid-js";
+import { useMounted } from "../use-mounted";
+
+/**
+ * SSR compatibility `isSupported`
+ */
+export function useSupported(callback: () => unknown): Accessor<boolean> {
+	const isMounted = useMounted();
+
+	return createMemo(() => {
+		isMounted();
+		return Boolean(callback());
+	});
+}
